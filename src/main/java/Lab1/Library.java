4ec127c947c766ef2e15f43f6e02147926c2077e
smart <smart@DESKTOP-20FORNJ>
2022-02-22 21:35:07 +0800

    Change 1

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -4,10 +4,16 @@
 package Lab1;
 
 public class Library {
+
+    public static void main(String[] args) {
+    	Library lib = new Library();
+    	if (lib.someLibraryMethod()) {
+        	System.out.println("Hello, World!");
+    	}
+    	
+    }
+    
     public boolean someLibraryMethod() {
         return true;
-    }
-    public static void main(String[] args) {
-    	System.out.println("Hello, World!");
     }
 }
